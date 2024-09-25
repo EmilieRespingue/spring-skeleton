87e43dfd52681489e0f67ea97df44b2e314a69f9
EmilieRespingue <respingue@gmail.com>
2024-09-25 22:26:44 +0200

    Added created user + fixed JUnit for SpringSecurity

--- skeleton/src/main/java/com/skeleton/SkeletonApplication.java
+++ skeleton/src/main/java/com/skeleton/SkeletonApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkeletonApplication {
-
+	// TODO: faire la BDD
+	// TODO: page de login, page de vraie inscription custom - en HTML
+	// TODO: rôle admin et user
 	public static void main(String[] args) {
 		SpringApplication.run(SkeletonApplication.class, args);
 	}
